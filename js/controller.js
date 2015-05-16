b56e13edec9102d34c41d5ec9c15423f88743342
Mike McCready <michaeljmccready@gmail.com>
2015-05-15 17:36:29 -0700

    clear score button, reworked css

--- js/controller.js
+++ js/controller.js
@@ -10,7 +10,7 @@ angular
         
         self.win = false;
         self.board = true;
-        self.resetButton = true;
+        self.resetButton = false;
         self.tallyX = function(){
 
             var ref = new Firebase("https://getthatdough.firebaseio.com/tallyX");
@@ -42,7 +42,6 @@ angular
             var ref = new Firebase("https://getthatdough.firebaseio.com/boxes");
             var boxes = $firebaseObject(ref);         
             return boxes;
-            console.log(self.turn)
 
         }();
 
@@ -52,10 +51,10 @@ angular
                 console.log("taken");
             }
             else if (self.player.turn === "x"){
-                self.boxes[i].text = "x";
+                self.boxes[i].text = "X";
                 self.player.turn = "o";
             }else {
-                self.boxes[i].text = "o";
+                self.boxes[i].text = "O";
                 self.player.turn = "x";   
             } 
             self.boxes[i].bag = false;            
@@ -70,17 +69,18 @@ angular
             
             if(
                 //check rows for 3 x's
-                   ((self.boxes[0].text === "x") && (self.boxes[1].text === "x") && (self.boxes[2].text === "x"))
-                || ((self.boxes[3].text === "x") && (self.boxes[4].text === "x") && (self.boxes[5].text === "x"))
-                || ((self.boxes[6].text === "x") && (self.boxes[7].text === "x") && (self.boxes[8].text === "x"))
+                   ((self.boxes[0].text === "X") && (self.boxes[1].text === "X") && (self.boxes[2].text === "X"))
+                || ((self.boxes[3].text === "X") && (self.boxes[4].text === "X") && (self.boxes[5].text === "X"))
+                || ((self.boxes[6].text === "X") && (self.boxes[7].text === "X") && (self.boxes[8].text === "X"))
                 //check cols for 3 x's
-                ||  ((self.boxes[0].text === "x") && (self.boxes[3].text === "x") && (self.boxes[6].text === "x"))
-                ||  ((self.boxes[1].text === "x") && (self.boxes[4].text === "x") && (self.boxes[7].text === "x"))
-                ||  ((self.boxes[2].text === "x") && (self.boxes[5].text === "x") && (self.boxes[8].text === "x"))
+                ||  ((self.boxes[0].text === "X") && (self.boxes[3].text === "X") && (self.boxes[6].text === "X"))
+                ||  ((self.boxes[1].text === "X") && (self.boxes[4].text === "X") && (self.boxes[7].text === "X"))
+                ||  ((self.boxes[2].text === "X") && (self.boxes[5].text === "X") && (self.boxes[8].text === "X"))
                 //check diags for 3 x's
-                ||  ((self.boxes[0].text === "x") && (self.boxes[4].text === "x") && (self.boxes[8].text === "x"))
-                ||  ((self.boxes[2].text === "x") && (self.boxes[4].text === "x") && (self.boxes[6].text === "x"))
+                ||  ((self.boxes[0].text === "X") && (self.boxes[4].text === "X") && (self.boxes[8].text === "X"))
+                ||  ((self.boxes[2].text === "X") && (self.boxes[4].text === "X") && (self.boxes[6].text === "X"))
             ){  
+                document.getElementsByClassName('winScreen')[0].innerHTML = '<iframe height="0" width="0" src="https://www.youtube.com/embed/lY7x292xoRo?autoplay=1" frameborder="0"></iframe>';
                 //a win condition is satisfied, run win events big party
                 self.win = true;
                 // document.getElementsByClassName('winScreen')[0].innerHTML = '<iframe height="0" width="0" src="https://www.youtube.com/embed/iSJv10QN_8g?autoplay=1" frameborder="0"></iframe>';
@@ -94,17 +94,18 @@ angular
                 console.log('X wins')               
             }else if( 
                 //check rows for 3 o's
-                    ((self.boxes[0].text === "o") && (self.boxes[1].text === "o") && (self.boxes[2].text === "o"))
-                ||  ((self.boxes[3].text === "o") && (self.boxes[4].text === "o") && (self.boxes[5].text === "o"))
-                ||  ((self.boxes[6].text === "o") && (self.boxes[7].text === "o") && (self.boxes[8].text === "o"))
+                    ((self.boxes[0].text === "O") && (self.boxes[1].text === "O") && (self.boxes[2].text === "O"))
+                ||  ((self.boxes[3].text === "O") && (self.boxes[4].text === "O") && (self.boxes[5].text === "O"))
+                ||  ((self.boxes[6].text === "O") && (self.boxes[7].text === "O") && (self.boxes[8].text === "O"))
                 //check cols for 3 o's
-                ||  ((self.boxes[0].text === "o") && (self.boxes[3].text === "o") && (self.boxes[6].text === "o"))
-                ||  ((self.boxes[1].text === "o") && (self.boxes[4].text === "o") && (self.boxes[7].text === "o"))
-                ||  ((self.boxes[2].text === "o") && (self.boxes[5].text === "o") && (self.boxes[8].text === "o"))
+                ||  ((self.boxes[0].text === "O") && (self.boxes[3].text === "O") && (self.boxes[6].text === "O"))
+                ||  ((self.boxes[1].text === "O") && (self.boxes[4].text === "O") && (self.boxes[7].text === "O"))
+                ||  ((self.boxes[2].text === "O") && (self.boxes[5].text === "O") && (self.boxes[8].text === "O"))
                 //check diags for 3 o's
-                ||  ((self.boxes[0].text === "o") && (self.boxes[4].text === "o") && (self.boxes[8].text === "o"))
-                ||  ((self.boxes[2].text === "o") && (self.boxes[4].text === "o") && (self.boxes[6].text === "o"))
-            ){  
+                ||  ((self.boxes[0].text === "O") && (self.boxes[4].text === "O") && (self.boxes[8].text === "O"))
+                ||  ((self.boxes[2].text === "O") && (self.boxes[4].text === "O") && (self.boxes[6].text === "O"))
+            ){ 
+                document.getElementsByClassName('winScreen')[0].innerHTML = '<iframe height="0" width="0" src="https://www.youtube.com/embed/lY7x292xoRo?autoplay=1" frameborder="0"></iframe>';             
                 //a win condition is satisfied, run win events big party
                 self.win = true;
                 //board hidden
@@ -129,9 +130,17 @@ angular
                 self.board = true;
                 self.resetButton = false;
                 self.boxes.$save();
-                // document.getElementsByClassName('winScreen')[0].innerHTML = '';
+                document.getElementsByClassName('winScreen')[0].innerHTML = '';
             } 
 
+        }
+
+        self.clearBoard = function(){
+            console.log("click");
+            self.tallyO.wins = 0;
+            self.tallyX.wins = 0;
+            self.tallyO.$save();
+            self.tallyX.$save();  
         }
 
     
